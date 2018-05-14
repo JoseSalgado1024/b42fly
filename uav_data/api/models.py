089a367226568167cb743c94b663cf0795351e72
jsalgados <jsalgadosalomon@anac.gob.ar>
2018-05-14 00:30:06 -0300

    Create models wip@

--- uav_data/api/models.py
+++ uav_data/api/models.py
@@ -1,3 +1,27 @@
-from django.db import models
+# from django.db import models
+from django.contrib.gis.db import models
+
 
 # Create your models here.
+class Restriction(models.Model):
+    """
+
+    """
+
+
+class ExtraField(models.Model):
+    """
+    Extra field
+    """
+
+
+class Zone(models.Model):
+    """
+
+    """
+    name = models.CharField('Zone name', max_length=10, default='')
+    description = models.CharField('Zone description', max_length=300, default='')
+    polygon = models.MultiPolygonField('Zone polygon')
+
+    def __str__(self):
+        return '' % self.name
